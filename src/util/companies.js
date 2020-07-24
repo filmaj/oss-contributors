27efe494f1847c8d7cac5d251d2b38bac1d2be34
filmaj <maj.fil@gmail.com>
2020-07-24 09:40:04 -0400

    more specific exclusion criteria for amazon - ignore anything that precedes or follows "aws" with a character

--- src/util/companies.js
+++ src/util/companies.js
@@ -126,7 +126,7 @@ module.exports = {
     },
     ignore: {
         'Cisco Systems': /francisco/i,
-        'Amazon': /paws|awska|amazonas|awssat|taws/i
+        'Amazon': /[a-z]aws|aws[a-z]|amazonas/i
     },
     catch_all: /microsoft|google|amazon|amzn|aws|red hat|redhat|ibm|pivotal|intel|facebook|alibaba|uber|wix|github|tencent|baidu|apple||mozilla|oracle|shopify|mongodb|vmware|netflix|salesforce|linkedin|palantir|yahoo|mapbox|unity|automattic|travis|spotify|zalando|esri|sap|epam|telerik|stripe|kitware|suse|odoo|yandex|adobe|airbnb|guardian|docker|nuxeo|nvidia|elastic|yelp|wso2|inria|puppet|datadog|jetbrains|canonical|rackspace|thoughtworks|andela|liferay|epfl|cnrs|embl|european bioinformatics|accenture|cisco|ericsson|capital one|huawei|booking\.com|netease|bbc|nokia|zendesk|paypal|eth z|samsung|slack|twitter|lyft|snapchat|zeit|atlassian|square|wechat|wikimedia|observable|magento|behance|aviary|fotolia/gi
 };
