e279f02729ec96d7a7b42f6e237dc92be2c7fb3e
filmaj <maj.fil@gmail.com>
2020-10-01 15:01:42 -0400

    use new ocotokit constructor approach

--- src/update-db.js
+++ src/update-db.js
@@ -10,7 +10,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-const Octokit = require('@octokit/rest');
+const {Octokit} = require('@octokit/rest');
 const {BigQuery} = require('@google-cloud/bigquery');
 const moment = require('moment');
 moment.relativeTimeThreshold('m', 55);
