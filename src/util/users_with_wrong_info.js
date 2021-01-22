03ecc39c638be91dc5bea2ba4813b11854747807
filmaj <maj.fil@gmail.com>
2021-01-22 16:18:58 -0600

    adding gitlab and grafana to company list, adding more users to the exception list

--- src/util/users_with_wrong_info.js
+++ src/util/users_with_wrong_info.js
@@ -15,22 +15,43 @@ governing permissions and limitations under the License.
 module.exports = {
     xmcclure: 'Mermaid Heavy Industries', // https://www.linkedin.com/in/andi-mcclure-47910835/
     'typescript-bot': 'bot', // not a human
+    tomerk: 'Google', // https://www.linkedin.com/in/tomerkaftan/
     timrisi: 'KickBack Rewards Systems', // https://www.linkedin.com/in/timothyrisi/
+    tanzhenyu: 'Google', // https://www.linkedin.com/in/zhenyu-tan-62499126/
+    suo: 'Facebook', // https://www.linkedin.com/in/michael-suo-a7624176/
+    smessmer: 'Facebook', // https://www.linkedin.com/in/sebastian-me%C3%9Fmer-077ab382/?originalSubdomain=de
     smartin821: 'Wrike', // https://www.linkedin.com/in/seanmartin2/
+    sjamesr: 'Google', // https://www.linkedin.com/in/james-ring-5b80aa8/
+    seemethere: 'Facebook', // https://www.linkedin.com/in/elias-uriegas/
+    rylev: 'Microsoft', // https://www.linkedin.com/in/ryanlevick/
+    ryantxu: 'Grafana Labs', // https://www.linkedin.com/in/ryan-mckinley/
     rdxvsrmv: 'bot', // not a human
+    pytorchbot: 'bot', // not a human
     purvilmehta06: 'Student', // https://www.linkedin.com/in/purvil-mehta/
+    pavithrasv: 'Google', // https://www.linkedin.com/in/pavithra-seshadri-vijayakrishnan-a100bb26/
+    papagian: 'Grafana Labs', // https://www.linkedin.com/in/sofia-papagiannaki/?originalSubdomain=gr
     opse2esa: 'bot', // not a human
     olprod: 'bot', // not a human
     'msft-github-bot': 'bot', // not a human
     microsoftopensource: 'bot', // not a human
+    jvishnuvardhan: 'Google', // https://www.linkedin.com/in/vishnuvardhanjanapati/
+    jongyllen: 'Grafana Labs', // https://www.linkedin.com/in/jongyllen/?originalSubdomain=se
+    ivanahuckova: 'Grafana Labs', // https://www.linkedin.com/in/ivana-huckova/
+    hugohaggmark: 'Grafana Labs', // https://www.hugohaggmark.com/experience/
     hubot: 'bot', // not a human
+    gottbrath: 'Facebook', // https://www.linkedin.com/in/chrisgottbrath/
+    goldiegadde: 'Google', // https://www.linkedin.com/in/goldie-gadde-266a4a4/
     glotbot: 'bot', // not a human
     gannonprudhomme: 'Student', // https://www.linkedin.com/in/gannon-prudhomme/
+    'facebook-circleci-bot': 'bot', // not a human
     'electron-bot': 'bot', // not a human
     cramforce: 'Google', // https://www.linkedin.com/in/malteubl/
+    briangann: 'Grafana Labs', // https://www.linkedin.com/in/bgann/
     betai: 'Google', // https://www.linkedin.com/in/betaikoffi/
     'azure-sdk': 'bot', // not a human
     awsmobilesdk: 'bot', // not a human
+    aocenas: 'Grafana Labs', // https://www.linkedin.com/in/andrej-ocenas-751b7520/?originalSubdomain=sk
+    annarev: 'Google', // http://annalabs.com/about/
     OPSTestPPE: 'bot', // not a human
     JunBinLiang: '', // https://www.linkedin.com/in/junbin-liang-482556176/
     Devneval: 'Lumilinks' // https://www.linkedin.com/in/coderdavid/
